b68b9fee4933a7f43ff365273113fb812ac02781
Prasanta Kumar Sen <156912990+prasantaelvira@users.noreply.github.com>
2025-06-01 11:48:24 +0530

    add,edit,delete done

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,36 @@ function App() {
     { id: 2, name: "Soap", price: 14.49 },
     { id: 3, name: "Tea", price: 39.99 },
   ]);
+  const [selectProduct, setSelectedProduct] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const handleAddProduct = () => {
     setShowModal(true);
+  };
+  const handleSaveProduct = (productData) => {
+    if (productData.id) {
+      setProducts((prev) =>
+        prev.map((eachElement) =>
+          eachElement.id == productData.id ? productData : eachElement
+        )
+      );
+    } else {
+      const newProduct = {
+        ...productData,
+        id: products.length
+          ? Math.max(...products.map((eachElement) => eachElement.id)) + 1
+          : 1,
+      };
+      setProducts((prev) => [...prev, newProduct]);
+    }
+  };
+  const handleEditProduct = (product) => {
+    setShowModal(true);
+    setSelectedProduct(product);
+  };
+  const handleDeleteProduct = (id) => {
+    setProducts((prev) => {
+      return prev.filter((eachElement) => eachElement.id !== id);
+    });
   };
   return (
     <>
@@ -59,11 +86,13 @@ function App() {
                       variant="outlined"
                       size="small"
                       startIcon={<EditIcon />}
+                      onClick={() => handleEditProduct(product)}
                     ></Button>
                     <Button
                       variant="outlined"
                       size="small"
                       startIcon={<DeleteIcon />}
+                      onClick={() => handleDeleteProduct(product.id)}
                     ></Button>
                   </Stack>
                 </TableCell>
@@ -73,7 +102,15 @@ function App() {
         </Table>
       </TableContainer>
 
-      <AddEditModal open={showModal} handleClose={()=>setShowModal(false)}/>
+      <AddEditModal
+        onSave={handleSaveProduct}
+        open={showModal}
+        handleClose={() => {
+          setShowModal(false);
+          setSelectedProduct(null);
+        }}
+        submittedProduct={selectProduct}
+      />
     </>
   );
 }
